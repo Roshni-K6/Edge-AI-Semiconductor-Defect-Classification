2ef9e2fd58db57295bc00ac814225725dcf1a8ad
Roshni-K6 <roshnik.ece2024@citchennai.net>
2026-02-08 17:34:59 +0530

    export_onnx.py

--- src/export_onnx.py
+++ src/export_onnx.py
@@ -1,31 +1,21 @@
+#PyTorch → ONNX
 import torch
 import torchvision.models as models
 
-# Device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Using device:", device)
-
-# Load trained model
-model = models.mobilenet_v2(pretrained=True)
-num_classes = 9
-in_features = model.classifier[1].in_features
-model.classifier[1] = torch.nn.Linear(in_features, num_classes)
-model.load_state_dict(torch.load("mobilenet_defect_model.pth", map_location=device))
-model = model.to(device)
+model = models.mobilenet_v2()
+model.classifier[1] = torch.nn.Linear(model.last_channel, 9)
+model.load_state_dict(torch.load("mobilenet_defect_model.pth"))
 model.eval()
 
-# Dummy input
-dummy_input = torch.randn(1, 3, 224, 224, device=device)
+dummy = torch.randn(1, 3, 224, 224)
 
-# Export to ONNX
 torch.onnx.export(
     model,
-    dummy_input,
-    "model/defect_classifier.onnx",
+    dummy,
+    "mobilenet_defect_model.onnx",
     input_names=["input"],
     output_names=["output"],
     opset_version=18
 )
 
-print("Model exported to ONNX successfully at model/defect_classifier.onnx")
-
+print("ONNX export successful")
